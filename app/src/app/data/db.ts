4dd2ddc510632233374862edb10f4562ddfd871c
Lulando <pawellulewicz39@gmail.com>
2023-03-12 17:25:36 +0100

    Update db.ts

--- app/src/app/data/db.ts
+++ app/src/app/data/db.ts
@@ -17,18 +17,23 @@ export const WORDS: Thing[] = [
     answer: undefined,
   },
   {
-    type: 'ROSLINA',
-    name: 'Róża',
-    answer: undefined,
-  },
-  {
     type: 'ZWIERZ',
     name: 'Szympans',
     answer: undefined,
   },
   {
     type: 'ROSLINA',
+    name: 'Róża',
+    answer: undefined,
+  },
+  {
+    type: 'ROSLINA',
     name: 'Lulek Czarny',
+    answer: undefined,
+  },
+  {
+    type: 'ZWIERZ',
+    name: 'Ropucha',
     answer: undefined,
   },
 ];
